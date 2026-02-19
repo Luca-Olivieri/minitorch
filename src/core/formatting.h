beef7b874058056995c97b475335c357aece50a0
Luca Olivieri <luca.olivieri@mail.polimi.it>
2026-02-19 18:12:45 +0100

    Implemented generic std::map printing.

--- src/core/formatting.h
+++ src/core/formatting.h
@@ -3,9 +3,10 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 template <typename T>
-inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector){
+inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector) {
     std::string out_string = "[";
     for (size_t i { 0 }; i < vector.size(); i++) {
         out_string += std::to_string(vector[i]);
@@ -16,5 +17,26 @@ inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector){
     out_string += "]";
     return os << out_string;
 }
+
+template <typename K, typename V>
+inline std::ostream& operator<<(
+        std::ostream& os,
+        const std::map<K, V>& map
+) {
+    os << "{\n";
+    for (auto it = map.cbegin(); it != map.cend(); ++it)
+    {
+        if (typeid(it->first) == typeid(std::string)) {
+            os << "    \"" <<  it->first << "\"";
+        }
+        else {
+            os << "    " <<  it->first;
+        }
+        os << ": " << it->second << ",\n";
+    }
+    os << "}\n";
+    return os;
+}
+
 
 #endif
