591840ff27862130e7ef78d3bf3240ceeeedada7
Luca Olivieri <luca.olivieri@mail.polimi.it>
2026-02-21 19:59:45 +0100

    Implemented covertype training loop.

--- src/main.cpp
+++ src/main.cpp
@@ -17,194 +17,61 @@
 
 namespace nn = mt::nn;
 
-Tensor forward_1st(
-    Tensor& inputs
-) {
-    Tensor exp_2 { inputs.shape(), 2.0f, false };
+void try_covertype() {
 
-    return inputs.pow(exp_2) - inputs;
-}
-
-void optimize_1st_deriv() {
-    Tensor x = Tensor::linspace({2, 3}, -1.0f, 2.0f);
-
-    Tensor lrs { x.shape(), 1e-2f };
-
-    for (size_t i = 0; i < 500; i++) {
-        Tensor o {forward_1st(x)};
-        o.backward();
-        x += -lrs * x.grad();
-        x.zero_grad();
-    }
-
-    std::cout << x << '\n';
-}
-
-void try_nn() {
-    Tensor x = Tensor::linspace({12}, 0.1f, 0.9f);
-    nn::Linear lin1(12, 18);
-    nn::kaiming_normal_inplace(lin1.m_weight, get_rng());
-    
-    nn::ReLU relu{};
-    Tensor y = lin1.forward(x);
-    Tensor z = relu.forward(y);
-    z.backward();
-    std::cout << x.grad() << '\n';
-    std::cout << lin1.m_weight.grad() << '\n';
-    std::cout << lin1.m_bias.grad() << '\n';
-}
-
-class IrisDataset: public mt::data::ClassificationDataset {
-public:
-    IrisDataset(
-        const std::string& path
-    ): m_csv_reader(path) {
-
-        m_len = m_csv_reader.size();
-
-        flower_to_class.emplace("Iris-setosa", 0.0f);
-        flower_to_class.emplace("Iris-versicolor", 1.0f);
-        flower_to_class.emplace("Iris-virginica", 2.0f);
-    }
-
-    std::tuple<Tensor, Tensor> getitem(
-            size_t index
-    ) {
-        std::string line = m_csv_reader.read_line(index);
-        std::stringstream ss(line);
-        std::string value;
-
-        Tensor input({4});
-        // discard 'Id' column
-        std::getline(ss, value, ',');
-
-        std::getline(ss, value, ',');
-        input[{0}] = std::stof(value);
-
-        std::getline(ss, value, ',');
-        input[{1}] = std::stof(value);
-        
-        std::getline(ss, value, ',');
-        input[{2}] = std::stof(value);
-        
-        std::getline(ss, value, ',');
-        input[{3}] = std::stof(value);
-
-        Tensor gt{std::vector<size_t>()};
-        std::getline(ss, value, ',');
-        gt.item() = flower_to_class.at(value);
-
-        return {input, gt};
-    }
-
-    size_t len() const {
-        return m_len;
-    }
-private:
-    io::CSVReader m_csv_reader;
-    std::map<std::string, float> flower_to_class;
-};
-
-void try_xor() {
-    
-    Tensor x({4, 2}, 0.0f, false);
-    x[{0, 0}] = 0.0f; x[{0, 1}] = 0.0f;
-    x[{1, 0}] = 0.0f; x[{1, 1}] = 1.0f;
-    x[{2, 0}] = 1.0f; x[{2, 1}] = 0.0f;
-    x[{3, 0}] = 1.0f; x[{3, 1}] = 1.0f;
-    
-    Tensor gts({4}, 0.0f, false);
-    gts[{0}] = 0.0f;
-    gts[{1}] = 1.0f;
-    gts[{2}] = 1.0f;
-    gts[{3}] = 0.0f;
-
-    class FFN: public nn::Module, nn::Forward1 {
+    class CovertypeDataset: public mt::data::ClassificationDataset {
     public:
-        nn::Linear lin1;
-        nn::ReLU relu;
-        nn::Linear lin2;
-        nn::Linear lin3;
+        CovertypeDataset(
+            const std::string& path
+        ): m_csv_reader(path) {
 
-        FFN():
-            lin1(2, 4),
-            relu{},
-            lin2(4, 3),
-            lin3(3, 1) {
-                register_module("lin1", lin1);
-                register_module("lin2", lin2);
-                register_module("lin3", lin3);
-                reset_parameters();
+            // m_len = m_csv_reader.size();
+            m_len = 1000;
+        }
+
+        std::tuple<Tensor, Tensor> getitem(
+                size_t index
+        ) {
+            std::string line = m_csv_reader.read_line(index);
+            std::stringstream ss(line);
+            std::string value;
+
+            Tensor input({54});
+            // discard 'index' column
+            std::getline(ss, value, ',');
+
+            for (size_t col {0}; col < 54; ++col) {
+                std::getline(ss, value, ',');
+                float f = std::stof(value);
+                input[{col}] = f;
             }
 
-        Tensor forward(
-                const Tensor& inputs
-        ) const {
-            Tensor y1 = lin1.forward(inputs);
-            Tensor y2 = relu.forward(y1);
-            Tensor y3 = lin2.forward(y2);
-            Tensor y4 = relu.forward(y3);
-            Tensor out = lin3.forward(y4);
-            Tensor prs = out.squeeze(1);
-            return prs;
+            Tensor gt{std::vector<size_t>()};
+            std::getline(ss, value, ',');
+            gt.item() = std::stof(value) - 1;
+
+            return {input, gt};
         }
 
-        void reset_parameters() {
-            nn::kaiming_uniform_inplace(lin1.m_weight, get_rng());
-            lin1.m_bias.fill_inplace(0.0f);
-            nn::kaiming_uniform_inplace(lin2.m_weight, get_rng());
-            lin2.m_bias.fill_inplace(0.0f);
-            nn::kaiming_uniform_inplace(lin3.m_weight, get_rng());
-            lin3.m_bias.fill_inplace(0.0f);
+        size_t len() const {
+            return m_len;
         }
+    private:
+        io::CSVReader m_csv_reader;
     };
 
-    FFN ffn{};
-
-    nn::BCELossWithLogits criterion{};
-    auto params = ffn.parameters();
-    std::cout << params << '\n';
-    SGD optimizer(params, config["base_lr"]);
-
-    {
-        Tensor prs_ = ffn.forward(x);
-        std::cout << prs_ << '\n';
-        
-        std::cout << criterion.forward(prs_, gts);
-    }
-
-    for (size_t i = 0; i < 2000; i++) {
-        Tensor prs = ffn.forward(x);
-
-        Tensor loss = criterion.forward(prs, gts);
-        loss.backward();
-        
-        optimizer.step();
-        optimizer.zero_grad();
-    }
-
-    {
-        Tensor prs_ = ffn.forward(x);
-        std::cout << prs_ << '\n';
-        
-        std::cout << criterion.forward(prs_, gts);
-    }
-}
-
-void try_iris() {
-
-    class IrisClassifier: public nn::Module, nn::Forward1 {
+    class CovertypeClassifier: public nn::Module, nn::Forward1 {
     public:
         nn::Linear lin1;
         nn::ReLU relu;
         nn::Linear lin2;
         nn::Linear lin3;
 
-        IrisClassifier():
-            lin1(4, 20),
+        CovertypeClassifier():
+            lin1(54, 100),
             relu{},
-            lin2(20,30),
-            lin3(30, 3) {
+            lin2(100, 100),
+            lin3(100, 7) {
                 register_module("lin1", lin1);
                 register_module("lin2", lin2);
                 register_module("lin3", lin3);
@@ -230,13 +97,38 @@ void try_iris() {
             nn::kaiming_uniform_inplace(lin3.m_weight, get_rng());
             lin3.m_bias.fill_inplace(0.0f);
         }
+
+        Tensor evaluate(
+            mt::data::DataLoader<Tensor, Tensor>& dl,
+            nn::Loss& criterion
+        ) {
+            float curr_loss = 0;
+            float curr_sample_count = 0;
+
+            for (size_t step { 0 }; step < dl.size(); ++step) {
+                auto [inputs, gts] = dl.get_batch(step);
+                
+                Tensor prs_oh = forward(inputs);
+
+                Tensor gts_oh = gts.one_hot(prs_oh.shape()[1]);
+            
+                Tensor loss = criterion.forward(prs_oh, gts_oh);
+
+                curr_loss += loss.item()*(static_cast<float>(inputs.shape()[0]));
+                curr_sample_count += static_cast<float>(inputs.shape()[0]);
+            }
+            float total_loss = curr_loss / curr_sample_count;
+            return Tensor({}, total_loss);
+        }
     };
 
-    IrisDataset ds(config["iris_path"]);
+    CovertypeDataset train_ds(config["covertype_train_path"]);
+    CovertypeDataset val_ds(config["covertype_val_path"]);
 
-    mt::data::DataLoader<Tensor, Tensor> dl(ds, 4, true, get_rng());
+    mt::data::DataLoader<Tensor, Tensor> train_dl(train_ds, 4, true, get_rng());
+    mt::data::DataLoader<Tensor, Tensor> val_dl(val_ds, 4, false, get_rng());
 
-    IrisClassifier model{};
+    CovertypeClassifier model{};
 
     nn::CrossEntropyLoss criterion{};
     auto params = model.parameters();
@@ -244,28 +136,34 @@ void try_iris() {
 
     const size_t num_epochs = 10;
 
+    std::cout << "Initial loss: " << model.evaluate(val_dl, criterion).item() << '\n';
+
     for (size_t epoch { 0 }; epoch < num_epochs; ++epoch) {
 
-        for (size_t step { 0 }; step < dl.size(); ++step) {
-            auto [inputs, gts] = dl.get_batch(step);
+        for (size_t step { 0 }; step < train_dl.size(); ++step) {
+            auto [inputs, gts] = train_dl.get_batch(step);
             
-            Tensor prs = model.forward(inputs);
+            Tensor prs_oh = model.forward(inputs);
 
-            Tensor gts_oh = gts.one_hot(3);
-        
-            Tensor loss = criterion.forward(prs, gts_oh);
+            Tensor gts_oh = gts.one_hot(prs_oh.shape()[1]);
+
+            Tensor loss = criterion.forward(prs_oh, gts_oh);
             loss.backward();
 
-            if (step % 10 == 0) {
-                std::cout << loss << '\n';
-            }
+            // if (step % 10 == 0) {
+            //     std::cout << loss << '\n';
+            // }
             
             optimizer.step();
             optimizer.zero_grad();
         }
+        
+        std::cout << std::format("[Epoch {}/{}] val. loss: {}", epoch+1, num_epochs, model.evaluate(val_dl, criterion).item()) << '\n';
 
-        dl.reshuffle();
+        train_dl.reshuffle();
     }
+
+    std::cout << "Final training loss: " << model.evaluate(train_dl, criterion).item() << '\n';
 }
 
 int main()
@@ -274,7 +172,8 @@ int main()
     // optimize_2nd_deriv();
     // try_nn();
     // try_xor();
-    try_iris();
+    // try_iris();
+    try_covertype();
 
     return 0;
 }
